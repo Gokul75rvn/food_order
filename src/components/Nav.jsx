5a8f5a0fbdbc69f7a5f8f398fbcdfa245c920e92
Gokul75rvn <gokulofficial75rvn@gmail.com>
2025-07-26 15:44:55 +0530

    updated

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import { HiOutlineHome } from "react-icons/hi2";
+import { BsExclamationDiamond } from "react-icons/bs";
+import { RiContactsLine } from "react-icons/ri";
+import { CiLogin } from "react-icons/ci";
+import { Link } from "react-router";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+function Nav() 
+{
+
+return <div className="nav">
+ <img className="logo" src="logo.jpg" alt="logo" />
+ <div className="nav-links">
+    <Link to="/" className="tx"><HiOutlineHome /> Home</Link>
+    <Link to="/About" className="tx"><BsExclamationDiamond />About</Link>
+    <Link to="/Contact" className="tx"><RiContactsLine />Contact</Link>
+ 
+    <SignedOut>
+                     <SignInButton />
+                 </SignedOut>
+                 <SignedIn>
+                     <UserButton />
+                 </SignedIn>
+  </div>
+</div>
+}
+export default Nav;
+
